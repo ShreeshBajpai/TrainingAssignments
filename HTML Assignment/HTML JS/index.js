463b1a200f51fa81693a92a7a72a7b2fa6869800
Shreesh <shreesh.dutta@gmail.com>
2022-08-12 21:07:58 +0530

    Javascript

---
+++ HTML Assignment/HTML JS/index.js
@@ -0,0 +1,17 @@
+function demo(){
+    document.write("hei i am using javascript function")
+    }
+function warning(){
+    alert("hey i am using javascript function")
+    }
+function identity(){
+    var x=document.getElementById("name").value;
+    alert("hey i am using javascript function ::"+x)
+}
+function display(){
+    alert("i am in ")
+    var fname=document.form1.fullname.value;
+    
+    var pwd=document.form1.password.value;
+  document.write("name  :"+fname+"/"+"/password  :"+pwd);
+}
